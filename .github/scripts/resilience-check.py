0dcf4178bdd7fc96ac9cff0914bfe9281e3cdaec
EC2 Default User <ec2-user@ip-172-31-29-131.ec2.internal>
2022-11-02 23:52:11 +0000

    test-23

--- .github/scripts/resilience-check.py
+++ .github/scripts/resilience-check.py
@@ -4,7 +4,7 @@ import os
 AppARN = os.environ['AppARN']
 StackARN = os.environ['StackARN']
 
-arh = boto3.client('resiliencehub')
+arh = boto3.client('resiliencehub', region_name='us-west-2')
 
 response = arh.import_resources_to_draft_app_version(
     appArn=AppARN,
