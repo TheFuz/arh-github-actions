1451992a861afacb77f2fdda9768ce8313c41bf0
EC2 Default User <ec2-user@ip-172-31-29-131.ec2.internal>
2023-02-03 15:53:41 +0000

    test

--- .github/scripts/resilience-check.py
+++ .github/scripts/resilience-check.py
@@ -1,65 +1,68 @@
 import boto3
 import os
 import time
-import sys
 
-AppARN = os.environ['AppARN']
-StackARN = os.environ['StackARN']
+def resilience_check():
+    app_arn = os.environ['AppARN']
+    StackARN = os.environ['StackARN']
 
-arh = boto3.client('resiliencehub', region_name='us-east-1')
+    arh = boto3.client('resiliencehub', region_name='us-east-1')
 
-import_resources = arh.import_resources_to_draft_app_version(
-    appArn=AppARN,
-    sourceArns=[
-        StackARN,
-    ]
-)
+    import_resources = arh.import_resources_to_draft_app_version(
+        appArn=app_arn,
+        sourceArns=[
+            StackARN,
+        ]
+    )
 
-import_status = arh.describe_draft_app_version_resources_import_status(
-    appArn=AppARN
-)
-
-print(import_status['status'])
-
-while (import_status['status'] == 'Pending' or import_status['status'] == 'InProgress'):
-    time.sleep(5)
     import_status = arh.describe_draft_app_version_resources_import_status(
-        appArn=AppARN
-    )
-    print(import_status['status'])
-    
-if import_status['status'] == 'Failed':
-    raise Exception('Resource import failed - ' + str(import_status))
-    
-publish_app = arh.publish_app_version(
-    appArn=AppARN
-)
-
-start_assessment = arh.start_app_assessment(
-        appArn=AppARN,
-        appVersion='release',
-        assessmentName='GitHub-actions-assessment'
+        appArn=app_arn
     )
 
-assessmentARN = start_assessment['assessment']['assessmentArn']
+    print('Waiting for import to complete, current status = ' + import_status['status'])
 
-assessment_status = arh.describe_app_assessment(
-    assessmentArn=assessmentARN
-)
+    while (import_status['status'] in {'Pending' or 'InProgress'}):
+        time.sleep(5)
+        import_status = arh.describe_draft_app_version_resources_import_status(
+            appArn=app_arn
+        )
+        print(import_status['status'])
+        
+    if import_status['status'] == 'Failed':
+        raise Exception('Resource import failed - ' + str(import_status))
+        
+    publish_app = arh.publish_app_version(
+        appArn=app_arn
+    )
 
-print(assessment_status['assessment']['assessmentStatus'])
+    start_assessment = arh.start_app_assessment(
+            appArn=app_arn,
+            appVersion='release',
+            assessmentName='GitHub-actions-assessment'
+        )
 
-while (assessment_status['assessment']['assessmentStatus'] == 'Pending' or assessment_status['assessment']['assessmentStatus'] == 'InProgress'):
-    time.sleep(5)
+    assessmentARN = start_assessment['assessment']['assessmentArn']
+
     assessment_status = arh.describe_app_assessment(
         assessmentArn=assessmentARN
-    )
-    print(assessment_status['assessment']['assessmentStatus'])
-    
-if assessment_status['assessment']['assessmentStatus'] == 'Failed':
-    raise Exception('Assessment failed - ' + str(assessment_status))
-    
-if assessment_status['assessment']['complianceStatus'] == 'PolicyBreached':
-    raise Exception('Policy breached')
-else:
-    print('Policy met')
+    )['assessment']['assessmentStatus']
+
+    print(assessment_status)
+
+    while (assessment_status in {'Pending' or 'InProgress'}):
+        time.sleep(5)
+        assessment_status = arh.describe_app_assessment(
+            assessmentArn=assessmentARN
+        )
+        print(assessment_status)
+        
+    if assessment_status == 'Failed':
+        raise Exception('Assessment failed - review the assessment on Resilience Hub for details.')
+        
+    if assessment_status == 'PolicyBreached':
+        raise Exception('Policy breached')
+    else:
+        print('Policy met')
+
+if __name__ == "__main__":
+    resilience_check()
